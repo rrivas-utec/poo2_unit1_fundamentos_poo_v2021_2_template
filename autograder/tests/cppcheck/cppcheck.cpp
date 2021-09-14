94ced4ff599004cf9639fef60e04162a9fed6a90
rrivas-utec <rrivas@utec.edu.pe>
2021-09-14 13:50:26 -0500

    Updated Tests

--- autograder/tests/cppcheck/cppcheck.cpp
+++ autograder/tests/cppcheck/cppcheck.cpp
@@ -1,9 +1,9 @@
 #include "../redirect_io.h"
-#include "../redirect_io.h"
 #include "../calculate_pentagon.h"
 #include "../number.h"
 #include "../quick_sort.h"
 #include "../search_odds.h"
+#include "../composite_figure.h"
 
   int main() {
     return 0;
